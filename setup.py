8fa5060770f68db0b16c00eef50f932f4bdeac54
Sofiaanjum <ahmedsofia15@gmail.com>
2023-08-07 22:06:42 -0400

    requirements, dependencies & installations done

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Humber-Summarizer"
+AUTHOR_USER_NAME = "Sofiaanjum"
+SRC_REPO = "Humber-Summarizer"
+AUTHOR_EMAIL = "ahmedsofia15@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP text summarization app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Corrected field name
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
